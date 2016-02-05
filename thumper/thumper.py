8b5868b327c94af59fae887e24d7ee88daa2e271
gmxu <georgemxu@gmail.com>
2016-02-05 11:23:20 -0500

    Update thumper.py

--- thumper/thumper.py
+++ thumper/thumper.py
@@ -1,11 +1,18 @@
+# Import socket IO for commucating with a web server, you should remove this for now since you don't have a
+# web server set up at the moment
 from socketIO_client import SocketIO
 
+# Import serial library
 import serial
 import threading
 
+# Open serial connection to the TRex connected to the USB, depending on the port used, run the command 'ls /dev/tty*' 
+# on the Pi, to find the exact name of the connection
+# 9600 is the baud rate for the communication, both the Pi and TRex must be listening for the same baud rate
 ser = serial.Serial("/dev/ttyUSB0", 9600)
 print 'Opened serial'
 
+# Set variables
 exploSpeedMin = 60
 exploSpeedMax = 80
 exploSpeed = 60
@@ -16,6 +23,7 @@ turningR = 0
 turningL = 0
 turningAngle = 50 # this is the difference between the speed of right and left motors
 
+# Define a listener for the web server to act on commands send from the web server
 def listener(*args):
     global exploSpeedMin
     global exploSpeedMax
@@ -24,7 +32,7 @@ def listener(*args):
     global goingBackward
     global turningR
     global turningL
-
+    
     # this is to calculate turning speed 
     if exploSpeed > exploSpeedMax - int(turningAngle / 2):
         topSpeed = exploSpeedMax
@@ -40,6 +48,8 @@ def listener(*args):
         print 'Move FORWARD'
 
         if turningR:
+            # Movement commands are sent in this format HSXXXSXXX where S is can either by 2 or 0 for forward or back and XXX
+            # goes from 0 to 255 to control speed, the pairs are for each pair of motors on the left and right sides of the robot
             ser.write('H' + chr(2) + chr(lowSpeed) + chr(2) + chr(topSpeed))
         elif turningL:
             ser.write('H' + chr(2) + chr(topSpeed) + chr(2) + chr(lowSpeed))
@@ -123,6 +133,7 @@ def listener(*args):
 
         socketIO.emit('thumperToServer', str(exploSpeed))
 
+# Open SocketIO connection to server
 socketIO = SocketIO('54.213.169.59', 3000)
 socketIO.on('serverToThumper', listener)
 socketIO.emit('clientType', 'Python')
